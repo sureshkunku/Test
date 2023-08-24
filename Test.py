33473bea34e2acd139dad02fbf2f21bdd49e1d27
Suresh Chowdary <sureshbabuk782@gmail.com>
2023-08-24 12:39:17 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ data_list = [
     {'key1': 'value3', 'key3': 'value3', 'key4': 'value5'},
 ]
 
-def print_table(data_list):
+def format_table(data_list):
     all_keys = set(key for data_dict in data_list for key in data_dict.keys())
     max_key_length = max(max(len(key) for key in all_keys), len("Keys"))
     table_format = "{:<{key_width}} |"
@@ -16,14 +16,20 @@ def print_table(data_list):
 
     header = table_format.format("Keys", key_width=max_key_length)
     columns = [key for key in all_keys]
-    print(header)
-    print(separator)
+
+    formatted_table = []
+    formatted_table.append(header)
+    formatted_table.append(separator)
+
     for data_dict in data_list:
         row = table_format.format("Data", key_width=max_key_length)
         for key in columns:
             value = data_dict.get(key, "")
             row += " {:<15} |".format(value)
-        print(row)
-        print(separator)
+        formatted_table.append(row)
+        formatted_table.append(separator)
 
-print_table(data_list)
+    return '\n'.join(formatted_table)
+
+formatted_output = format_table(data_list)
+print(formatted_output)
