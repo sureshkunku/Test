443d9dc7b716046b83a68f82c3f3f874582ab9f2
Suresh Chowdary <sureshbabuk782@gmail.com>
2023-10-04 09:30:39 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,30 +1,70 @@
-<body>
-    <form id="myForm" action="your_upload_handler.php" method="POST" enctype="multipart/form-data">
-        <label>
-            <input type="radio" name="fileType" value="option1" id="option1"> Option 1 (Mandatory)
-        </label>
-        <label>
-            <input type="radio" name="fileType" value="option2" id="option2"> Option 2 (Optional)
-        </label>
-        <br>
-        <input type="file" name="file" id="fileInput" style="display: none;">
-        <label for="fileInput" id="fileInputLabel" style="cursor: pointer;">Choose a File</label>
-        <br>
-        <input type="submit" value="Upload">
-    </form>
+Response received from Mayo Clinic.
 
-    <script>
-        const option1Radio = document.getElementById('option1');
-        const fileInput = document.getElementById('fileInput');
+Gartner Research Board - Consult the Board Survey
 
-        option1Radio.addEventListener('change', function () {
-            if (option1Radio.checked) {
-                fileInput.style.display = 'block';
-                fileInput.required = true;
-            } else {
-                fileInput.style.display = 'none';
-                fileInput.required = false;
-            }
-        });
-    </script>
-</body>
+Software Asset Management Practices
+
+1. 1. What is your enterprise's level of maturity with software asset management (SAM)?
+Initial: SAM strategies and practices are new and/or undocumented. (If selected, you have completed the survey. Please skip to the end and hit submit.)
+ 
+2. 2. Today, which of the following best describes how your SAM governance is set up?
+N/A
+
+ 
+3. 3. Which of the following suppliers, if any, do you leverage for SAM capabilities? Please select all that apply. 
+N/A
+
+ 
+4. 4. What SAM tools, if any, are you using for accountability and/or monitoring of license consumption? Please describe them below.
+
+
+
+(Accountability in this context refers to the practice of review of requests for disposition, licensing, tracking against a threshold allocation, utilization, and entitlement/reclamation.)
+N/A
+
+ 
+5a. 5. SAM Effectiveness:
+
+
+
+a. How would you rate the overall effectiveness of your enterprise's SAM practices?
+
+N/A
+
+ 
+5b. b. What is your rationale for the answer you selected above?
+N/A
+
+ 
+6. 6. What roles and/or responsibilities has your enterprise identified as critical to your SAM program? Please list and describe them below.
+N/A
+
+ 
+7a. 7. SAM program challenges:
+
+
+
+a. Please describe what (if any) challenges your organization has faced implementing your SAM program.
+
+N/A
+
+ 
+7b. b. Based on the challenges you described above, what advice would you give to address those challenges?
+N/A
+
+ 
+8. 8. Is there anything we should know about how to interpret or treat your response or any additional context you would like to share with us about this topic? If so, please tell us below. 
+N/A
+
+ 
+9. 9. Would you or someone from your organization be willing to speak with the sponsoring organization about your response? If yes, please provide the following information for the appropriate contact from your organization.
+Name:	
+N/A
+
+Title:	
+N/A
+
+Email address:	
+N/A
+
+ 
